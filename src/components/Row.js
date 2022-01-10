c41ae8346b3c8642011ab616939109e5b9a40d29
Saara Leppis <saara@leppihalme.fi>
2022-01-10 10:24:32 +0200

    got functions work, added rat and cheese

--- src/components/Row.js
+++ src/components/Row.js
@@ -7,13 +7,17 @@ function Row({ maze }) {
     return (
         <div>
             {maze.map((row, index) => {
+                const N = maze.length;
                 return (
                     <div className='row-wrap'>
                         {row.map((cell, subindex) =>
-                            <Cell fill={cell === 1 ? "free" :
-                                cell === 2 ? "green" :
-                                    cell === 3 ? "rat" :
-                                        'not-free'} key={index * Math.random()} />)}
+                            <Cell fill={
+                                index === 0 && subindex === 0 ? "rat" :
+                                    index === N - 1 && subindex === N - 1 ? "cheese" :
+                                        cell === 1 ? "free" :
+                                            cell === 2 ? "green" :
+                                                cell === 3 ? "rat" :
+                                                    'not-free'} key={index * Math.random()} />)}
                     </div>)
             })}
         </div>
